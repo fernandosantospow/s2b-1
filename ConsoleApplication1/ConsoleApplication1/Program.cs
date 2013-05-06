0f75e5aa10e09b944a99a9173b3df9226380eae2
Diego Lopes <diego.lopes@travelexplorer.com.br>
2013-05-06 14:04:59 -0300

    commit inicial
    commit inicial da aplicação

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b, total;
+
+            a = "Visual";
+            b = "Studio";
+
+            total = a + b;
+
+            Console.WriteLine(total.Length.ToString());
+            Console.ReadKey();
+        }
+    }
+}
